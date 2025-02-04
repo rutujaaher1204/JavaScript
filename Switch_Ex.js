3d51b26fa719eba313d0b15a395f3ee6909813e6
rutujaaher1204 <161233988+rutujaaher1204@users.noreply.github.com>
2025-02-04 16:27:33 +0530

    Add files via upload

---
+++ Switch_Ex.js
@@ -0,0 +1,22 @@
+let day=parseInt(prompt("Enter a number (1 -7) for the day of the week"));
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    default :
+        console.log("Weekend or invalid day");
+        break;
+
+}
