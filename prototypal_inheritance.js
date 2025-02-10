8d8f89687c21cf7eebfdbb96379873a02cdae830
rutujaaher1204 <161233988+rutujaaher1204@users.noreply.github.com>
2025-02-10 12:33:53 +0530

    Add files via upload

---
+++ prototypal_inheritance.js
@@ -0,0 +1,11 @@
+//prototypal inheritance 
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.speak = function() {
+    console.log(`${this.name} makes a noise.`);
+};
+
+const dog = new Animal("Dog");
+dog.speak(); 
